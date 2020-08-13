52d1b0d27721912571dab7f262efee720dcecb72
Deadvilla97 <65792729+Deadvilla97@users.noreply.github.com>
2020-08-12 20:47:59 -0500

    Digital-Clock

---
+++ app.js
@@ -0,0 +1,20 @@
+function clock() {
+    var fullDate = new Date();
+    var hours = fullDate.getHours();
+    var minute = fullDate.getMinutes();
+    var secs = fullDate.getSeconds();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (hours < 10) {
+        minute = "0" + minute;
+    }
+    if (secs < 10) {
+        secs = "0" + sec;
+    }
+    document.getElementById('hour').innerHTML = hours;
+    document.getElementById('minute').innerHTML = ":" + minute;
+    document.getElementById('second').innerHTML = ":" + secs;
+}
+
+setInterval(clock, 1000);
